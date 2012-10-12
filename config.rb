348b4451d19e9a12a2b0fb7c66ac593998417cf9
Daren <daren@dgimedia.co.uk>
2012-10-12 21:08:55 +0100

    new direction new model

--- config.rb
+++ config.rb
@@ -1,4 +1,6 @@
+require 'zurb-foundation'
 # Require any additional compass plugins here.
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -7,15 +9,14 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
 
-output_style = :nested
-environment = :development
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-color_output = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
